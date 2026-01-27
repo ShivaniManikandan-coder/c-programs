68de57e28e71809554da164853272441e11f6544
Shivani Manikandan <shivanimanikandan235@gmail.com>
2026-01-27 23:21:51 +0530

    Add files via upload
    mobile warranty

---
+++ mobile-warnity.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int warrantyCategory;
+    scanf("%d", &warrantyCategory);
+    switch (warrantyCategory) {
+        case 1:  
+            printf("Under Warranty");
+            break;
+        case 2: 
+            printf("Limited Warranty");
+            break;
+        case 3:  
+            printf("Out of Warranty");
+            break;
+        default:
+            break;
+    }
+
+    return 0;
+}
