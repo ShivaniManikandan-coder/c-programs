ddaa36a56eb7bae071f44c546a5ad12cf3aa75be
Shivani Manikandan <shivanimanikandan235@gmail.com>
2026-01-27 22:40:36 +0530

    Add files via upload
    water bill calculation

---
+++ switchcase-waterbill.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int connectionType, units;
+    int bill = 0;
+    scanf("%d %d", &connectionType, &units);
+    switch (connectionType) {
+        case 1: 
+            if (units <= 30) {
+                bill = units * 5;
+            } else {
+                bill = 30 * 5 + (units - 30) * 8;
+            }
+            break;
+
+        case 2:  
+            bill = units * 10;
+            break;
+
+        default:  
+            return 0;
+    }
+
+    printf("Bill %d", bill);
+
+    return 0;
+}
