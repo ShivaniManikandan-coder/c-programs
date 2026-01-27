1f5330a7b4863a1dce242d4a1bc1fbfd41da6ec8
Shivani Manikandan <shivanimanikandan235@gmail.com>
2026-01-27 22:26:53 +0530

    Add files via upload
    employee salary calculation

---
+++ switchcase-salary.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int code, experience;
+    int baseSalary = 0;
+    int bonus = 0;
+    int totalSalary = 0;
+
+    
+    scanf("%d", &code);
+    scanf("%d", &experience);
+
+    switch (code) {
+        case 1:  
+            baseSalary = 50000;
+            break;
+        case 2: 
+            baseSalary = 35000;
+            break;
+        default:
+            return 0;
+    }
+
+    if (experience >= 3) {
+        bonus = experience * 5000;
+    }
+
+    totalSalary = baseSalary + bonus;
+
+    printf("Salary %d", totalSalary);
+
+    return 0;
+}
