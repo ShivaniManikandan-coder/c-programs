381b133aa0559f9b7a7c7473f48e9e47c3822352
Shivani Manikandan <shivanimanikandan235@gmail.com>
2026-01-28 19:36:38 +0530

    Add files via upload
    factorial calculator

---
+++ problem6.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int N, i;
+    int factorial = 1;
+
+    scanf("%d", &N);
+
+    for(i = 1; i <= N; i++) {
+        factorial = factorial * i;
+    }
+
+    printf("%d", factorial);
+
+    return 0;
+}
+
